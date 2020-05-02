90790c54cd71800266c3dda66679b338d06c9901
polgasull <pgasulln@gmail.com>
2020-05-02 09:53:12 +0200

    Mobile resize

--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import { isMobile } from 'react-device-detect';
 
 import styles from './Tile.module.scss'
 
 const Tile = ({index, pos, onClick, toggleClass}) => {
-  const top = pos[0]*100 + 5;
-  const left = pos[1]*100 + 5;
+  const top = isMobile ? pos[0]*73 + 5 : pos[0]*100 + 5;
+  const left = isMobile ? pos[1]*73 + 5 : pos[1]*100 + 5;
   const bgLeft = (index%4)*100 + 5;
   const bgTop = Math.floor(index/4)*100 + 5;
 
